5a3318f0be06064b49514ae25edd45cd405d4693
Jirka <del@hs-furtwangen.de>
2018-04-28 18:03:36 +0200

    Done first version, works with Heroku

--- Client.js
+++ Client.js
@@ -1,8 +1,11 @@
 var ServerSentEventsTest;
 (function (ServerSentEventsTest) {
     window.addEventListener("load", init);
+    let server = "http://localhost:8100";
+    server = "https://serversentevents.herokuapp.com/";
     function init(_event) {
-        var eventSource = new EventSource("http://localhost:8100");
+        var eventSource = new EventSource(server);
+        console.log("connect to: " + server);
         eventSource.addEventListener("test", handleServerSentEvent);
     }
     function handleServerSentEvent(_event) {
--- Client.ts
+++ Client.ts
@@ -1,8 +1,11 @@
 namespace ServerSentEventsTest {
     window.addEventListener("load", init);
+    let server: string = "http://localhost:8100";
+    server = "https://serversentevents.herokuapp.com/";
 
     function init(_event: Event): void {
-        var eventSource: EventSource = new EventSource("http://localhost:8100");
+        var eventSource: EventSource = new EventSource(server);
+        console.log("connect to: " + server);
         eventSource.addEventListener("test", handleServerSentEvent);
     }
 
